a62e72ac05df0210dd048fbbd5e2e964e9956a2d
Saqib <20-43435-1@student.aiub.edu>
2024-07-17 10:37:37 +0600

    contentscript.js updated

--- contentScript.js
+++ contentScript.js
@@ -1,10 +1,7 @@
 
-
 chrome.runtime.onMessage.addListener( async (message, sender, sendResponse) => {
-    if (message.message) {
-      document.body.innerText = "This Site is Blocked By You. Now You are being redirect to www.google.com"   
-      setTimeout(() => {
-        window.location.replace('http://www.google.com')
-      }, 4000);
-    }
-  });
+  if (message.message) {
+    document.body.innerHTML = '<div style="display: flex; align-items: center; justify-content: center; height: 100vh; font-size: 24px; color: red; text-align: center;">This Site is Blocked By You.</div>';
+
+  }
+});
